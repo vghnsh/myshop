c6b500bdfca3bf9a181f724a0d2a3e8976610217
vghnsh <vighneshchaurasia17@gmail.com>
2020-07-05 20:28:08 +0530

    First Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-
+import { Switch,Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+const Hotsp = (props) => {
+  console.log(props);
+  return(
+  <div>
+    <h1>OP bolte</h1>
+
+  </div>
+  );
+};
 
 function App() {
   return (
     <div >
-      <HomePage/>
+      <Switch>
+        <Route  exact path='/' component={HomePage}/>
+        <Route  path='/hats' component={Hotsp}/>
+      </Switch>
+      
     </div>
   );
 }
