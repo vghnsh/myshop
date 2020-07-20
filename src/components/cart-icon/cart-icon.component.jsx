5520565c7e785bcf2e7714343059ba88a5e8ce1e
vghnsh <vighneshchaurasia17@gmail.com>
2020-07-20 13:12:57 +0530

    added selectors in cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {connect} from 'react-redux';
 import {toggleCartHidden} from '../../redux/cart/cart.action';
+import { selectCartItemsCount } from '../../redux/cart/cart.selector';
 
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
@@ -18,12 +19,9 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden:()=>dispatch(toggleCartHidden())
 });
 
-const mapStateToProps=({cart: {cartItems}}) => ({
-    itemCount: cartItems.reduce(
-        (accumalatedQunatity,cartItem)=>
-    accumalatedQunatity + cartItem.quantity,
-    0
-    )
+const mapStateToProps=state => ({
+    itemCount:  selectCartItemsCount(state)
+    
 });
 
 export default connect( 
